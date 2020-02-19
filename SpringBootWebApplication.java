92204a0881ceca46c55083214637b210a5bac245
Amel94 <53213321+Amel94@users.noreply.github.com>
2020-02-20 00:53:48 +1100

    initial commit of Spring boot main class

---
+++ SpringBootWebApplication.java
@@ -0,0 +1,28 @@
+package lk.dialog.ist.reslo;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+	
+	
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+	
+	
+
+}
